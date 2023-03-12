d40dabd6f77494878e5ac7c0d0213d987e166d4d
Kee-Reall <kirill_bezrodny@mail.ru>
2023-03-13 00:49:13 +0500

    e#2

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	fmt.Println(st)
 	addTwo(&st)
 	fmt.Println(st)
+	crt()
 }
 
 func addTwo(num *int) {
@@ -21,4 +22,14 @@ func addTwo(num *int) {
 func cursorPrint(curs **int) {
 	fmt.Println(curs)
 	fmt.Println(&curs)
+}
+
+func crt() {
+	var i int16 = 0
+	for true {
+		print(i)
+		if i == 32 {
+			break
+		}
+	}
 }
