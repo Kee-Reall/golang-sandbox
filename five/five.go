b5422b08b18e631d299aea954dcdf1e880492f95
Kee-Reall <kirill_bezrodny@mail.ru>
2023-04-07 23:53:01 +0500

    done

---
+++ five/five.go
@@ -0,0 +1,12 @@
+package five
+
+import "errors"
+
+//some comments
+//need to export
+func Five(num int) (bool, error) {
+	if num > 5 {
+		return true, nil
+	}
+	return false, errors.New("number more than 5")
+}
--- main.go
+++ main.go
@@ -2,55 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
-	"net/http"
+	"sandbox/five"
 )
 
-var db = make(map[string]string)
-
-func setupRouter() *gin.Engine {
-
-	r := gin.Default()
-
-	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-		"foo":  "bar", // user:foo password:bar
-		"manu": "123", // user:manu password:123
-	}), gin.Bind(func(c *gin.Context) {
-		ip := c.ClientIP()
-		fmt.Print("request from ip %s", ip)
-	}))
-
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
-
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := db[user]
-		if ok {
-			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
-		}
-	})
-
-	authorized.POST("admin", func(c *gin.Context) {
-		user := c.MustGet(gin.AuthUserKey).(string)
-
-		var json struct {
-			Value string `json:"value" binding:"required"`
-		}
-
-		if c.Bind(&json) == nil {
-			db[user] = json.Value
-			c.JSON(http.StatusOK, gin.H{"status": "ok"})
-		}
-	})
-
-	return r
-}
-
 func main() {
-	r := setupRouter()
-	r.Run(":8080")
+	fmt.Print(five.Five(4))
 }
